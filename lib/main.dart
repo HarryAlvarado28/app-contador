ed4ffde888ef85c0a8d7d24b65d091c438e8462c
Harry Alvarado <harry2alvarado8@gmail.com>
2020-09-13 19:53:03 -0500

    Limpiando Codigo, y Agregando ICON en la APP

--- lib/main.dart
+++ lib/main.dart
@@ -96,71 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    // List<bool> _selections = [true, false];
-    // List<bool> _selections1 = [true, false];
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     // Here we take the value from the MyHomePage object that was created by
-    //     // the App.build method, and use it to set our appbar title.
-    //     title: Text(widget.title),
-    //   ),
-    //   body: Center(
-    //     // Center is a layout widget. It takes a single child and positions it
-    //     // in the middle of the parent.
-    //     child: Column(
-    //       // Column is also a layout widget. It takes a list of children and
-    //       // arranges them vertically. By default, it sizes itself to fit its
-    //       // children horizontally, and tries to be as tall as its parent.
-    //       //
-    //       // Invoke "debug painting" (press "p" in the console, choose the
-    //       // "Toggle Debug Paint" action from the Flutter Inspector in Android
-    //       // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-    //       // to see the wireframe for each widget.
-    //       //
-    //       // Column has various properties to control how it sizes itself and
-    //       // how it positions its children. Here we use mainAxisAlignment to
-    //       // center the children vertically; the main axis here is the vertical
-    //       // axis because Columns are vertical (the cross axis would be
-    //       // horizontal).
-    //       mainAxisAlignment: MainAxisAlignment.center,
-    //       children: <Widget>[
-    //         Text(
-    //           'Conteo:',
-    //         ),
-    //         Text(
-    //           '$_counter',
-    //           style: Theme.of(context).textTheme.headline4,
-    //         ),
-    //         ToggleButtons(
-    //           children: [
-    //             Icon(Icons.remove),
-    //             Icon(Icons.remove),
-    //           ],
-    //           isSelected: _selections,
-    //           onPressed: (int index) {
-    //             _decrementCounter();
-    //             print('El print INDEX ---> $index');
-    //             _selections[index] = !_selections[index];
-    //           },
-    //         ),
-    //         // ToggleButtons(
-    //         //   children: [Icon(Icons.plus_one)],
-    //         //   isSelected: _selections1,
-    //         //   onPressed: (int index) {
-    //         //     _incrementCounter();
-    //         //     print('El print INDEX ---> $index');
-    //         //     _selections1[index] = !_selections1[index];
-    //         //   },
-    //         // ),
-    //       ],
-    //     ),
-    //   ),
-    //   floatingActionButton: FloatingActionButton(
-    //     onPressed: _incrementCounter,
-    //     tooltip: 'Increment',
-    //     child: Icon(Icons.add),
-    //   ), // This trailing comma makes auto-formatting nicer for build methods.
-    // );
+
     var _dialVisible = true;
     return Scaffold(
       appBar: AppBar(
@@ -195,35 +131,9 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline1,
             ),
-            // ToggleButtons(
-            //   children: [
-            //     Icon(Icons.remove),
-            //     Icon(Icons.remove),
-            //   ],
-            //   isSelected: _selections,
-            //   onPressed: (int index) {
-            //     _decrementCounter();
-            //     print('El print INDEX ---> $index');
-            //     _selections[index] = !_selections[index];
-            //   },
-            // ),
-            // ToggleButtons(
-            //   children: [Icon(Icons.plus_one)],
-            //   isSelected: _selections1,
-            //   onPressed: (int index) {
-            //     _incrementCounter();
-            //     print('El print INDEX ---> $index');
-            //     _selections1[index] = !_selections1[index];
-            //   },
-            // ),
           ],
         ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
       floatingActionButton: SpeedDial(
         // both default to 16
         marginRight: 18,
@@ -308,59 +218,5 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
     );
-
-    // return build(context);
   }
 }
-
-// Widget build(BuildContext context) {
-//   var _dialVisible = true;
-//   return Scaffold(
-//     floatingActionButton: SpeedDial(
-//       // both default to 16
-//       marginRight: 18,
-//       marginBottom: 20,
-//       animatedIcon: AnimatedIcons.menu_close,
-//       animatedIconTheme: IconThemeData(size: 22.0),
-//       // this is ignored if animatedIcon is non null
-//       // child: Icon(Icons.add),
-//       visible: _dialVisible,
-//       // If true user is forced to close dial manually
-//       // by tapping main button and overlay is not rendered.
-//       closeManually: false,
-//       curve: Curves.bounceIn,
-//       overlayColor: Colors.black,
-//       overlayOpacity: 0.5,
-//       onOpen: () => print('OPENING DIAL'),
-//       onClose: () => print('DIAL CLOSED'),
-//       tooltip: 'Speed Dial',
-//       heroTag: 'speed-dial-hero-tag',
-//       backgroundColor: Colors.white,
-//       foregroundColor: Colors.black,
-//       elevation: 8.0,
-//       shape: CircleBorder(),
-//       children: [
-//         SpeedDialChild(
-//             child: Icon(Icons.accessibility),
-//             backgroundColor: Colors.red,
-//             label: 'First',
-//             labelStyle: TextStyle(fontSize: 18.0),
-//             onTap: () => print('FIRST CHILD')),
-//         SpeedDialChild(
-//           child: Icon(Icons.brush),
-//           backgroundColor: Colors.blue,
-//           label: 'Second',
-//           labelStyle: TextStyle(fontSize: 18.0),
-//           onTap: () => print('SECOND CHILD'),
-//         ),
-//         SpeedDialChild(
-//           child: Icon(Icons.keyboard_voice),
-//           backgroundColor: Colors.green,
-//           label: 'Third',
-//           labelStyle: TextStyle(fontSize: 18.0),
-//           onTap: () => print('THIRD CHILD'),
-//         ),
-//       ],
-//     ),
-//   );
-// }
